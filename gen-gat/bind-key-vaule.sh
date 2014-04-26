a6733f56cca9c53c0c1f8fb5e6a3acb1b8cf570d
Gnat TANG <gnat_tang@yeah.net>
2014-04-26 17:38:31 +0800

    vaule to value, fixed typo

--- gen-gat/bind-key-vaule.sh
+++ gen-gat/bind-key-vaule.sh
@@ -4,4 +4,4 @@ keys="gat-tables/output/gat-char-keys-6000.txt"
 values="zh_chars/zh_chars_6000.dat"
 
 
-pr -m -t -s\  ${keys} ${values} | gawk '{print $1,$2}' > gat-key-vaule-6000.txt
+pr -m -t -s\  ${keys} ${values} | gawk '{print $1,$2}' > gat-key-value-6000.txt
